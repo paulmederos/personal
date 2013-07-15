9aa4fe9d6de2e7fac6edc7818a3ca531e363b4bc
Paul Mederos <paulmederos@me.com>
2013-07-15 15:22:40 -0700

    Refactored footer. Added new blog post. Links to blogs now in footer.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Paulmederos::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#focus-or-discipline'
+  root 'pages#side-project-or-real-product'
 
   get '/adventures', to: 'pages#portfolio' 
 
-  get '/focus-or-disciple', to: 'pages#focus-or-disciple' 
+  get '/focus-or-discipline', to: 'pages#focus-or-discipline' 
   get '/helping-others-builds-reputation', to: 'pages#helping-others-builds-reputation'
+  get '/side-project-or-real-product', to: 'pages#side-project-or-real-product'
 end
