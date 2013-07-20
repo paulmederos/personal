2934656d246c6256f03959e3bdf8e3ade32403c8
Paul Mederos <paulmederos@me.com>
2013-07-19 19:09:45 -0700

    Updated routes, added new blog on designing for Growth mindset.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Paulmederos::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#side-project-or-real-product'
+  root 'pages#designing_for_growth_mindset'
 
   get '/adventures', to: 'pages#portfolio' 
 
-  get '/focus-or-discipline', to: 'pages#focus-or-discipline' 
-  get '/helping-others-builds-reputation', to: 'pages#helping-others-builds-reputation'
-  get '/side-project-or-real-product', to: 'pages#side-project-or-real-product'
+  get '/designing-for-growth-mindset', to: 'pages#designing_for_growth_mindset' 
+  get '/focus-or-discipline', to: 'pages#focus_or_discipline' 
+  get '/helping-others-builds-reputation', to: 'pages#helping_others_builds_reputation'
+  get '/side-project-or-real-product', to: 'pages#side_project_or_real_product'
 end
