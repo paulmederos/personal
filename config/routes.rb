7c8e7ae6ce9a3dca41879deb6301ea35e5748fd2
Paul Mederos <paulmederos@me.com>
2013-10-15 20:10:15 -0700

    How to eat blog added.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Paulmederos::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#designing_for_growth_mindset'
+  root 'pages#how_to_eat'
 
   get '/adventures', to: 'pages#portfolio' 
 
@@ -12,5 +12,6 @@ Paulmederos::Application.routes.draw do
   get '/helping-others-builds-reputation', to: 'pages#helping_others_builds_reputation'
   get '/side-project-or-real-product', to: 'pages#side_project_or_real_product'
   get '/diminishing-returns-behavior-change', to: 'pages#diminishing_returns'
+  get '/how-to-eat', to: 'pages#how-to-eat' 
   
 end
